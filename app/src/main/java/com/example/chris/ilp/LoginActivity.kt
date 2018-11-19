d0f7f36f6084f2faa964ae46c29b1c7650d46b2c
ClowDragon <43343691+ClowDragon@users.noreply.github.com>
2018-11-19 02:34:48 +0000

    complete register and login
    updated UI

--- app/src/main/java/com/example/chris/ilp/LoginActivity.kt
+++ app/src/main/java/com/example/chris/ilp/LoginActivity.kt
@@ -3,8 +3,10 @@ package com.example.chris.ilp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.chris.ilp.R.*
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
@@ -20,26 +22,40 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-
         email = findViewById(id.email) as EditText
         password = findViewById(id.password) as EditText
         loginButton = findViewById(id.loginButton) as Button
         registerButton = findViewById(id.registerButton) as Button
-
         registerButton.setOnClickListener{
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(intent)
         }
-
         loginButton.setOnClickListener{
+            if (TextUtils.isEmpty(email.text.toString())) {
+                Toast.makeText(applicationContext, "Enter email address!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(password.text.toString())) {
+                Toast.makeText(applicationContext, "Enter password!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
             auth = FirebaseAuth.getInstance()
             auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString()).
                     addOnCompleteListener { task: Task<AuthResult> ->
-                        val intentToMain = Intent(this@LoginActivity, MainActivity::class.java)
-                        startActivity(intentToMain)
+                        if (!task.isSuccessful) run {
+                            // there was an error
+                            if (password.length() < 6) {
+                                password.setError(getString(R.string.minimum_password))
+                            } else {
+                                Toast.makeText(this@LoginActivity, getString(R.string.auth_failed), Toast.LENGTH_LONG).show()
+                            }
+                        }
+                        else{
+                            val intentToMain = Intent(this@LoginActivity, MainActivity::class.java)
+                            startActivity(intentToMain)
+                        }
                     }
-
         }
     }
 }
--- app/src/main/java/com/example/chris/ilp/RegisterActivity.kt
+++ app/src/main/java/com/example/chris/ilp/RegisterActivity.kt
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import android.text.TextUtils
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var displayName: EditText
@@ -43,17 +46,38 @@ class RegisterActivity : AppCompatActivity() {
 
 
         registerButton.setOnClickListener(){
-            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString()).
+            if (TextUtils.isEmpty(displayName.text.toString())) {
+                Toast.makeText(applicationContext, "Enter your name!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(email.text.toString())) {
+                Toast.makeText(applicationContext, "Enter email address!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(password.text.toString())) {
+                Toast.makeText(applicationContext, "Enter password!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.length() < 6) {
+                Toast.makeText(applicationContext, "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            else{
+                auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString()).
                     addOnCompleteListener { task: Task<AuthResult> ->
-                        if (task.isSuccessful){
+                        if (task.isSuccessful) {
+                            Toast.makeText(applicationContext, "Account Created!", Toast.LENGTH_SHORT).show()
                             val userId = auth.currentUser?.uid
                             val registerRef = dbRef.child("user").child(userId)
                             val user = User(displayName.text.toString())
                             val intentToLoginActivity = Intent(this@RegisterActivity, LoginActivity::class.java)
                             startActivity(intentToLoginActivity)
-
                         }
                     }
+            }
         }
     }
 }
